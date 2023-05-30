8d338841b8751d7ab118b0d471dd4da54122bf3d
renz pajarito <85199262+RenzPajarito@users.noreply.github.com>
2023-05-30 13:21:20 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -27,3 +27,33 @@ function generateMessage() {
 // Add event listener to the button
 var generateButton = document.getElementById("generateBtn");
 generateButton.addEventListener("click", generateMessage);
+
+// Fluid Player
+var myFP = fluidPlayer("video", {
+  layoutControls: {
+    controlBar: {
+      autoHideTimeout: 3,
+      animated: true,
+      autoHide: true,
+    },
+    htmlOnPauseBlock: {
+      html: null,
+      height: null,
+      width: null,
+    },
+    autoPlay: false,
+    mute: false,
+    allowTheatre: true,
+    playPauseAnimation: true,
+    playbackRateEnabled: true,
+    allowDownload: false,
+    playButtonShowing: true,
+    fillToContainer: false,
+    posterImage: "images/gna9.png",
+  },
+  vastOptions: {
+    adList: [],
+    adCTAText: false,
+    adCTATextPosition: "Renz",
+  },
+});
